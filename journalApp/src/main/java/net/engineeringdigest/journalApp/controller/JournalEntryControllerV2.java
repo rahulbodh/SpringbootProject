e603c3788ea7aa04639052a1947da8a06a964c6d
Rahul <rahulbodh819@gmail.com>
2025-01-06 22:55:46 +0530

    ResponseEntity used for show status with status code

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
@@ -5,13 +5,12 @@ import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
@@ -23,43 +22,58 @@ public class JournalEntryControllerV2 {
     private JournalEntryService journalEntryService;
 
     @GetMapping
-    public List<JournalEntry> getAllJournalEntries() {
-        return journalEntryService.getAll();
+    public ResponseEntity<?> getAllJournalEntries() {
+        List<JournalEntry> allEntries = journalEntryService.getAll();
+        if(allEntries != null && !allEntries.isEmpty()) {
+            return new ResponseEntity<>(allEntries , HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry myEntry){
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry(myEntry);
-        return true;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+        try {
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry , HttpStatus.CREATED);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId) {
-        return journalEntryService.getById(myId).orElse(null);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
+        Optional<JournalEntry> journalEntry = journalEntryService.getById(myId);
+        if(journalEntry.isPresent()) {
+            return new ResponseEntity<>(journalEntry.get() , HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
-    public void deleteJournalEntryById(@PathVariable ObjectId myId) {
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId) { // ? - wild card pattern here , we can wrap another class object to the response entity
          journalEntryService.deleteById(myId);
+         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/all")
-    public boolean deleteAllEntries() {
+    public ResponseEntity<?> deleteAllEntries() {
         journalEntryService.deleteAll();
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("id/{myId}")
-    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId , @RequestBody JournalEntry myEntry) {
+    public ResponseEntity<?> updateJournalEntryById(@PathVariable ObjectId myId , @RequestBody JournalEntry myEntry) {
         JournalEntry oldEntry = journalEntryService.getById(myId).orElse(null);
         if (oldEntry != null) {
           oldEntry.setTitle(myEntry.getTitle() != null || myEntry.getTitle().equals("") ? myEntry.getTitle() : oldEntry.getTitle());
           oldEntry.setContent(myEntry.getContent() != null || myEntry.getContent().equals("") ? myEntry.getContent() : oldEntry.getContent());
+            journalEntryService.saveEntry(oldEntry);
+            return new ResponseEntity<>(oldEntry , HttpStatus.OK);
         }
-        journalEntryService.saveEntry(oldEntry);
-        return oldEntry;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
